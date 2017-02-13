dba00a0043374d26deda4c28b146d13c040109ac
User <User@PC-LITE>
2017-02-14 02:02:31 +0300

    refactoring

--- TelegramBotFirstApp/Program.cs
+++ TelegramBotFirstApp/Program.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
-using SimpleJSON;
+
 using System.Threading;
 
 namespace TelegramBotFirstApp
 {
+    public delegate void Response(object sendrer, ParameterResponse e); // Delegate response
     class Program
     {
         static void Main(string[] args)
@@ -18,50 +19,16 @@ namespace TelegramBotFirstApp
             Thread th = new Thread(Tr.GetUpdates);
             Tr.GetUpdates();
         }
-
         private static void Tr_ResponseReceived(object sendrer, ParameterResponse e)
         {
-            Console.WriteLine("{0} {1}: {2}   chatID:{3}",e.name, e.FirstName,e.message, e.chatID);
-        }
-
-        public delegate void Response(object sendrer, ParameterResponse e); // Delegate response
-        public class ParameterResponse:EventArgs
-        {
-            public string name;
-            public string FirstName;
-            public string message;
-            public string chatID;
-        }
-
-        class Telegram
-        {
-            int LastUpdateID = 0;
-            private string _token = "275547698:AAFqRX-HIAy-z62gSEQMZYHRvE28TM_vBEw";
-            private string LINK = "https://api.telegram.org/bot";
-            public event Response ResponseReceived; // событие для ответа
-            ParameterResponse e = new ParameterResponse();
-            public void GetUpdates()
-            {
-                while (true)
-                {
-                    using (WebClient webClient = new WebClient())
-                    {
-                        string response = webClient.DownloadString(LINK+ _token + "/getupdates?offset=" + (LastUpdateID+1));
-                        if (response.Length <= 23)
-                            continue;
-                        var N = JSON.Parse(response);
-                        foreach (JSONNode r in N["result"].AsArray)
-                        {
-                            LastUpdateID = r["update_id"].AsInt;
-                            e.FirstName = r["message"]["from"]["last_name"];
-                            e.name = r["message"]["from"]["first_name"];
-                            e.message = r["message"]["text"];
-                            e.chatID = r["message"]["chat"]["id"];
-                        }
-                    }
-                    ResponseReceived(this, e);
-                }
-            }
+            Console.WriteLine("{0} {1}: {2}   chatID:{3}", e.name, e.FirstName, e.message, e.chatID);
         }
     }
+    public class ParameterResponse : EventArgs
+    {
+        public string name;
+        public string FirstName;
+        public string message;
+        public string chatID;
+    }  
 }
---
+++ TelegramBotFirstApp/Telegram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleJSON;
+using System.Net;
+
+namespace TelegramBotFirstApp
+{
+    class Telegram
+    {
+        int LastUpdateID = 0;
+        private string _token = "275547698:AAFqRX-HIAy-z62gSEQMZYHRvE28TM_vBEw";
+        private string LINK = "https://api.telegram.org/bot";
+        public event Response ResponseReceived; // событие для ответа
+        ParameterResponse e = new ParameterResponse();
+        public void GetUpdates()
+        {
+            while (true)
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    string response = webClient.DownloadString(LINK + _token + "/getupdates?offset=" + (LastUpdateID + 1));
+                    if (response.Length <= 23)
+                        continue;
+                    var N = JSON.Parse(response);
+                    foreach (JSONNode r in N["result"].AsArray)
+                    {
+                        LastUpdateID = r["update_id"].AsInt;
+                        e.FirstName = r["message"]["from"]["last_name"];
+                        e.name = r["message"]["from"]["first_name"];
+                        e.message = r["message"]["text"];
+                        e.chatID = r["message"]["chat"]["id"];
+                    }
+                }
+                ResponseReceived(this, e);
+            }
+        }
+    }
+}
