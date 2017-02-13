69c12b1368878ef309287a706186afdba370b4e6
User <User@PC-LITE>
2017-02-14 01:58:08 +0300

    Fixed code

--- TelegramBotFirstApp/Program.cs
+++ TelegramBotFirstApp/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using SimpleJSON;
+using System.Threading;
 
 namespace TelegramBotFirstApp
 {
@@ -14,40 +15,45 @@ namespace TelegramBotFirstApp
         {
             Telegram Tr = new Telegram();
             Tr.ResponseReceived += Tr_ResponseReceived;
+            Thread th = new Thread(Tr.GetUpdates);
             Tr.GetUpdates();
         }
 
         private static void Tr_ResponseReceived(object sendrer, ParameterResponse e)
         {
-            Console.WriteLine("{0}: {1}   chatID:{2}",e.name,e.message, e.chatID);
+            Console.WriteLine("{0} {1}: {2}   chatID:{3}",e.name, e.FirstName,e.message, e.chatID);
         }
 
         public delegate void Response(object sendrer, ParameterResponse e); // Delegate response
         public class ParameterResponse:EventArgs
         {
             public string name;
+            public string FirstName;
             public string message;
             public string chatID;
         }
 
         class Telegram
         {
-            public string token = "token";
             int LastUpdateID = 0;
+            private string _token = "275547698:AAFqRX-HIAy-z62gSEQMZYHRvE28TM_vBEw";
+            private string LINK = "https://api.telegram.org/bot";
             public event Response ResponseReceived; // событие для ответа
             ParameterResponse e = new ParameterResponse();
-
             public void GetUpdates()
             {
                 while (true)
                 {
                     using (WebClient webClient = new WebClient())
                     {
-                        string response = webClient.DownloadString("https://api.telegram.org/bot"+ token + "/getupdates");
+                        string response = webClient.DownloadString(LINK+ _token + "/getupdates?offset=" + (LastUpdateID+1));
+                        if (response.Length <= 23)
+                            continue;
                         var N = JSON.Parse(response);
                         foreach (JSONNode r in N["result"].AsArray)
                         {
                             LastUpdateID = r["update_id"].AsInt;
+                            e.FirstName = r["message"]["from"]["last_name"];
                             e.name = r["message"]["from"]["first_name"];
                             e.message = r["message"]["text"];
                             e.chatID = r["message"]["chat"]["id"];
@@ -57,5 +63,5 @@ namespace TelegramBotFirstApp
                 }
             }
         }
-        }
+    }
 }
